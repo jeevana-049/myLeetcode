03f88b92dcd5a4ad42a2ee4c3c733e0b8d3e2f18
jeevana-049 <jeevanareddy004@gmail.com>
2026-07-25 10:31:27 +0530

    Time: 0 ms (100%), Space: 43.7 MB (11.1%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int c0 = 0;
+        int c1 = 0;
+        int c2 = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0) c0++;
+            else if(nums[i] == 1) c1++;
+            else c2++;
+        }
+        int k = 0;
+        for(int i = 0; i < c0; i++) {
+            nums[k++] = 0;
+        }
+        for(int i = 0; i < c1; i++) {
+            nums[k++] = 1;
+        }
+        for(int i = 0; i < c2; i++) {
+            nums[k++] = 2;
+        }
+    }
+}
