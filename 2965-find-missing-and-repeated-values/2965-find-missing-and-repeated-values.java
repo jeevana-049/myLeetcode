fbf457b56d4caed4b0e51d2290f5e92512cf9af0
jeevana-049 <jeevanareddy004@gmail.com>
2026-08-10 13:24:20 +0530

    Time: 5 ms (40.97%), Space: 47.3 MB (77.27%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
@@ -2,20 +2,22 @@ class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
         int repeated = 0;
         int missing = 0;
+        int sum = 0;
         Set<Integer> st = new HashSet<>();
         for(int i = 0; i < grid.length; i++) {
             for(int j = 0; j < grid[i].length; j++) {
                 int ele = grid[i][j];
+                sum += ele;
                 if(st.contains(ele)) repeated = ele;
-                else st.add(ele);
+                else {
+                    st.add(ele);
+                }
             }
         }
-        for(int i = 1; i <= grid.length * grid.length; i++) {
-            if(!st.contains(i)) {
-                missing = i;
-                break;
-            }
-        }
+        //expectedSum + repeated - missing = actualSum
+        int n = grid.length;
+        int expS = ((n * n) * ((n * n) + 1)) / 2;
+        missing = expS + repeated - sum;
         return new int[]{repeated, missing};
     }
 }
