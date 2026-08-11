cc4d01c3f2458d22b3305744688c278ea676d645
jeevana-049 <jeevanareddy004@gmail.com>
2026-08-11 22:26:06 +0530

    Time: 0 ms (100%), Space: 48.1 MB (47.81%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double mp = 1;
+        long N = n;
+        if(n < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        while(N > 0) {
+            if(N % 2 != 0) mp *= x;
+            x = (x * x);
+            N /= 2;
+        }
+        return mp;
+    }
+}
