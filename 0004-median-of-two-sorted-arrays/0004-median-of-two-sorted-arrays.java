294d589503c62a284a41a41a608c5b76c68e62d9
jeevana-049 <jeevanareddy004@gmail.com>
2026-09-12 20:11:29 +0530

    Time: 1 ms (100%), Space: 48.9 MB (32.17%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -3,25 +3,24 @@ class Solution {
         int n = nums1.length;
         int m = nums2.length;
         int o = m + n;
-        double[] a = new double[o];
+        int[] a = new int[o];
         int i = 0;
         int j = 0;
         int k = 0;
         while(i < n && j < m) {
             if(nums1[i] < nums2[j]) {
-                a[k++] = (double) nums1[i++];
+                a[k++] = nums1[i++];
             }
             else {
-                a[k++] = (double) nums2[j++];
+                a[k++] = nums2[j++];
             }
         }
-        while(i < n) a[k++] = (double) nums1[i++];
-        while(j < m) a[k++] = (double) nums2[j++];
+        while(i < n) a[k++] = nums1[i++];
+        while(j < m) a[k++] = nums2[j++];
         double med = 0.0;
         if(o % 2 == 0) {
-            med = ((a[(o - 1) / 2] + a[o / 2])) / 2;
+            return ((a[(o / 2) - 1] + a[o / 2])) / (double) 2;
         }
-        else med = a[o / 2];
-        return med;
+        return a[o / 2];
     }
 }
